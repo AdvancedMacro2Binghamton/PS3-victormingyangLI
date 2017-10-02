dfcb7ace1629bbc32ead067e9374a03bd8089c24
Mingyang Li <victormingyangli@foxmail.com>
2017-10-02 14:21:56 -0400

    PS3

--- PS3.m
+++ PS3.m
@@ -7,6 +7,7 @@ sigma = 1.5; % coefficient of risk aversion
 b = 0.5; % replacement ratio (unemployment benefits)
 y_s = [1, b]; % endowment in employment states
 PI = [.97 .03; .5 .5]; % transition matrix
+y = y_s(1); % set the initial endowment to 1 (when employed)
 
 % ASSET VECTOR
 % Why do we have the lower and upper bounds of asset?
@@ -40,18 +41,38 @@ while abs(aggsav) >= 0.01 ;
     v_tol = 1;
     while v_tol >.0001;
         % CONSTRUCT RETURN + EXPECTED CONTINUATION VALUE
-        
+        % generate a random number as the probability of changing state
+        X = rand;
+        % compute the utility value for all possible combinations of a and a' with e or u:
+        if y == y_s(1)
+            value_mat = ret + beta * (PI(1,1) * repmat(1 * permute(v_guess, [3 2 1]), [num_a 1]) + PI(1,2) * repmat(b * permute(v_guess, [3 2 1]), [num_a 1]));
+            if X > PI(1, 1)
+                y = y_s(2);
+            end
+        else
+            value_mat = ret + beta * (PI(2,1) * repmat(1 * permute(v_guess, [3 2 1]), [num_a 1]) + PI(2,2) * repmat(b * permute(v_guess, [3 2 1]), [num_a 1]));
+            if X < PI(2, 1)
+                y = y_s(1);
+            end
+        end
         % CHOOSE HIGHEST VALUE (ASSOCIATED WITH a' CHOICE)
+        [vfn, pol_indx] = max(value_mat, [], 2);
+        vfn = permute(vfn, [3 1 2]);
         
+        % what is the distance between current guess and value function
+        v_tol = max(abs(vfn - v_guess));
         
-  
+        % if distance is larger than tolerance, update current guess and
+        % continue, otherwise exit the loop
+        v_guess = vfn;
     end;
     
     % KEEP DECSISION RULE
     pol_fn = a(pol_indx);
     
     % SET UP INITITAL DISTRIBUTION
-
+    % What's the stucture and value of Mu?
+    Mu = ones(1, num_a, 2); % by Mingyang
     
     % ITERATE OVER DISTRIBUTIONS
     [emp_ind, a_ind, mass] = find(Mu > 0); % find non-zero indices
@@ -62,6 +83,8 @@ while abs(aggsav) >= 0.01 ;
         MuNew(:, apr_ind) = MuNew(:, apr_ind) + ... % which mass of households goes to which exogenous state?
             (PI(emp_ind(ii), :) * mass)';
     end
- 
+    
+    % calculate aggregate asset
+    aggsav = sum(a);
         
 end
